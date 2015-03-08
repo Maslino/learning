01fb66ed9101dea80c994c2b305077c1ae5bf1c0
maslino <liuxionghust@gmail.com>
2015-03-08 14:12:46 +0800

    fix django 1.7 specified model loading error

--- ipython_imports.py
+++ ipython_imports.py
@@ -1,6 +1,10 @@
 # coding=utf8
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning.settings")
+# if following two lines omitted, django 1.7 error:
+# AppRegistryNotReady: Models aren't loaded yet
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 from django.contrib.auth.models import User
 from blog.models import *
