b328dccf37411d920468ee7f3045f84ca86ceb66
liuxiong <liuxiong@qiyi.com>
2015-04-02 18:13:39 +0800

    add help text

--- blog/form.py
+++ blog/form.py
@@ -4,11 +4,11 @@ from video.common import is_url_supported
 
 
 class QRCodeForm(forms.Form):
-    url = forms.URLField(label=u'请输入网址')
+    url = forms.URLField(label=u'请输入网址', help_text=u'比如http://curvelearning.net')
 
 
 class VideoResolveForm(forms.Form):
-    url = forms.URLField(label=u'请输入视频网站播放页地址')
+    url = forms.URLField(label=u'请输入视频网站播放页地址', help_text=u'比如http://www.iqiyi.com/v_19rrnm5guk.html')
 
     def clean_url(self):
         url = self.cleaned_data['url']
